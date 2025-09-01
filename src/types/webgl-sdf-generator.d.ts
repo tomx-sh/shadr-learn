c194bec4974222acf369b5ef5074dc8f67ac7900
Thomas <33961528+tomx-sh@users.noreply.github.com>
2025-09-01 16:25:04 +0200

    Disable typescript rule to fix build

--- src/types/webgl-sdf-generator.d.ts
+++ src/types/webgl-sdf-generator.d.ts
@@ -1,4 +1,5 @@
 declare module "webgl-sdf-generator" {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const mod: any;
     export = mod; // works for both CJS/ESM at call sites
 }
